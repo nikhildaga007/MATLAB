7a6062a30fc2981f26b415929ce0951a4a488a48
Nikhil Daga <53420562+nikhildaga007@users.noreply.github.com>
2019-09-27 10:45:51 +0530

    Add files via upload

---
+++ lineareqations.m
@@ -0,0 +1,16 @@
+a=input('enter a : ');
+b=input('enter b : ');
+c=input('enter c : ');
+a1=input('enter a1 : ');
+b1=input('enter b1 : ');
+c1=input('enter c1 : ');
+a2=input('enter a2 : ');
+b2=input('enter b2 : ');
+c2=input('enter c2 : ');
+d1=input('enter d1 : ');
+d2=input('enter d2 : ');
+d3=input('enter d3 : ');
+A=[a b c;a1 b1 c1;a2 b2 c2];
+B=[d1 d2 d3]';
+X=A\B;
+
